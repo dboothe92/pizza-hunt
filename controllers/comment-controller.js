02a47e6c067b39d788281e66dadf701c147ef96d
Dan B <dbraunby@hotmail.com>
2021-05-20 10:47:45 -0600

    added reply functionality

--- controllers/comment-controller.js
+++ controllers/comment-controller.js
@@ -20,8 +20,23 @@ const commentController = {
             })
             .catch(err => res.json(err));
     }, 
+    addReply({ params, body }, res) {
+        Comment.findOneAndUpdate(
+            { _id: params.commentId },
+            { $push: { replies: body } },
+            { new: true }
+        )
+        .then(dbPizzaData => {
+            if (!dbPizzaData) {
+                res.status(404).json({ message: 'No pizza found with this id!'});
+                return;
+            }
+            res.json(dbPizzaData)
+        })
+        .catch(err => res.json(err))
+    },
     removeComment({ params }, res) {
-        Comment.findOneAndDelete({ _id: params.commentId})
+        Comment.findOneAndDelete({ _id: params.commentId })
             .then(deletedComment => {
                 if(!deletedComment) {
                     res.status(404).json({ message: "No comment found with this id!" });
@@ -41,6 +56,15 @@ const commentController = {
                 res.json(dbPizzaData);
             })
             .catch(err => res.json(err));
+    }, 
+    removeReply({ params }, res) {
+        Comment.findOneAndUpdate(
+            { _id: params.commentId },
+            { $pull: { replies: { replyId: params.replyId } } },
+            { new: true }
+        )
+        .then(dbPizzaData => res.json(dbPizzaData))
+        .catch(err => res.json(err));
     }
 };
 
